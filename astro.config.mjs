65a3481652c5091682a8386b0514266693f85c6b
kennethjulianda1207@gmail.com <kennethjulianda1207@gmail.com>
2025-06-18 13:46:14 +0800

    Fix configuration: restore base path in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io'
-//   base: '/cbc-website/',
+  site: 'https://astronaut.github.io',
+  base: '/cbc-website/',
 });
